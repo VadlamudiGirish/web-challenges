b71bd3ef5857d3d4f272cd5316666ee478ac8a15
Girish Vadlamudi <girishvadlamudi@Girishs-MacBook-Pro.local>
2025-03-11 10:42:09 +0100

    Implement js basic-functions

--- js-functions/js-functions_basic-functions/js/index.js
+++ js-functions/js-functions_basic-functions/js/index.js
@@ -9,7 +9,10 @@ console.clear();
 */
 
 // --v-- write your code here --v--
-
+const title = "Unfinished!";
+const author = "Priyanaka Chopra";
+let rating = 4.5;
+let numberOfCopiesSold = 20000;
 // --^-- write your code here --^--
 
 /*
@@ -27,6 +30,20 @@ Then:
 */
 
 // --v-- write your code here --v--
+console.log("Title: ", title);
+console.log("Author: ", author);
+console.log("Rating: ", rating);
+console.log("Sales: ", numberOfCopiesSold);
+
+rating += 0.25;
+numberOfCopiesSold += 590;
+
+console.log("After update:");
+
+console.log("Title: ", title);
+console.log("Author: ", author);
+console.log("Rating: ", rating);
+console.log("Sales: ", numberOfCopiesSold);
 
 // --^-- write your code here --^--
 
@@ -40,5 +57,19 @@ Then:
 */
 
 // --v-- write your code here --v--
+function logBookData() {
+  console.log("Title: ", title);
+  console.log("Author: ", author);
+  console.log("Rating: ", rating);
+  console.log("Sales: ", numberOfCopiesSold);
+}
+
+logBookData();
+
+numberOfCopiesSold += 600;
+logBookData();
+
+numberOfCopiesSold += 600;
+logBookData();
 
 // --^-- write your code here --^--
