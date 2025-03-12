3bb5aa258943c6fcb837a44a954f5a110158be05
Girish Vadlamudi <girishvadlamudi@Girishs-MacBook-Pro.local>
2025-03-12 11:06:31 +0100

    Implement change box appearence

--- js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,22 @@
 console.clear();
+
+const box = document.querySelector('[data-js="box"]');
+const inputColor = document.querySelector('[data-js="input-color"]');
+const inputRadius = document.querySelector('[data-js="input-radius"]');
+const inputRotation = document.querySelector('[data-js="input-rotation"]');
+
+inputColor.addEventListener("input", () => {
+  const hue = inputColor.value;
+  box.style.backgroundColor = `hsl(${hue}, 50%, 50%)`;
+});
+
+inputRadius.addEventListener("input", () => {
+  box.style.borderRadius =
+    Number(inputRadius.value) === Number(inputRadius.max)
+      ? "100%"
+      : `${inputRadius.value}px`;
+});
+
+inputRotation.addEventListener("input", () => {
+  box.style.transform = `rotate(${inputRotation.value}deg)`;
+});
