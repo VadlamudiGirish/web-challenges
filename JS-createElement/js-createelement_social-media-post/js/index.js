0b34d3358b426b49630bc49b028b5beb98a58042
Girish Vadlamudi <girishvadlamudi@Girishs-MacBook-Pro.local>
2025-03-17 15:14:43 +0100

    Implement JS-Create-Element

--- JS-createElement/js-createelement_social-media-post/js/index.js
+++ JS-createElement/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,29 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+const bodyElement = document.body;
+
+const articleElement = document.createElement("article");
+articleElement.className = "post";
+
+const paraElement = document.createElement("p");
+paraElement.className = "post__content";
+paraElement.textContent =
+  "orem ipsum dolor, sit amet consectetur adipisicing elit. ";
+
+const footerElement = document.createElement("footer");
+footerElement.className = "post__footer";
+
+const spanElement = document.createElement("span");
+spanElement.className = "post__username";
+spanElement.textContent = "@username";
+
+const buttonElement = document.createElement("button");
+buttonElement.className = "post__button";
+buttonElement.textContent = " ♥ Like";
+
+articleElement.append(paraElement, footerElement);
+footerElement.append(spanElement, buttonElement);
+bodyElement.append(articleElement);
+
+buttonElement.addEventListener("click", handleLikeButtonClick);
