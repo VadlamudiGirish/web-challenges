00a144d9caf2ebb25a352475b2a7e2896676e33f
Girish Vadlamudi <girishvadlamudi@Girishs-MacBook-Pro.local>
2025-03-17 16:19:32 +0100

    Implement while-random-number

--- js-loops/js-loops_while-random-number/index.js
+++ js-loops/js-loops_while-random-number/index.js
@@ -4,7 +4,10 @@ let number = 0;
 let count = 0;
 
 // --v-- write or modify code below this line --v--
-
+while (number < 0.9) {
+  count++;
+  number = Math.random();
+}
 // --^-- write or modify code above this line --^--
 
 console.log(
