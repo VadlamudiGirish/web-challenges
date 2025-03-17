44145f7425fb3fd68b8380a4b05b9dd5849b4b60
Girish Vadlamudi <girishvadlamudi@Girishs-MacBook-Pro.local>
2025-03-17 16:42:18 +0100

    Implement js-loops_stars

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,25 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
+  for (let i = 1; i <= 5; i++) {
+    const imgElement = document.createElement("img");
 
+    if (i <= filledStars) {
+      imgElement.setAttribute("src", "assets/star-filled.svg");
+    } else {
+      imgElement.setAttribute("src", "assets/star-empty.svg");
+    }
+
+    imgElement.addEventListener("click", () => renderStars(i));
+
+    starContainer.append(imgElement);
+  }
   // --^-- write or modify code above this line --^--
 }
 
-renderStars();
+renderStars(3);
