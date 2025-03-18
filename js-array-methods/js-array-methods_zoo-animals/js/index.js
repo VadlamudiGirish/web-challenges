19b8a818cbdd6111a39eea0226840bb24018ff52
Girish Vadlamudi <girishvadlamudi@Girishs-MacBook-Pro.local>
2025-03-18 12:39:49 +0100

    Implement zoo animals

--- js-array-methods/js-array-methods_zoo-animals/js/index.js
+++ js-array-methods/js-array-methods_zoo-animals/js/index.js
@@ -5,7 +5,11 @@ import { zooAnimals } from "../utils/db.js";
 // The function should return 'true' if the animal name is included in the array or 'false' if not.
 
 function hasAnimal(animals, animalName) {
-  return null;
+  const matchingAnimals = animals
+    .map((animal) => animal.toLowerCase())
+    .filter((animal) => animal === animalName.toLowerCase());
+
+  return matchingAnimals.length > 0;
 }
 
 // Bonus:
