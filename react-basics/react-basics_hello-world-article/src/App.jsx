ea42d55afeac6f7ad1cdaa44aa2ac26773dec577
Girish Vadlamudi <girishvadlamudi@Girishs-MacBook-Pro.local>
2025-03-25 17:08:37 +0100

    Implement hello-world-article

--- react-basics/react-basics_hello-world-article/src/App.jsx
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,14 @@
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <>{HelloWorldArticle()}</>;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World!!!</h1>
+      <p>My second React Component</p>
+    </article>
+  );
 }
